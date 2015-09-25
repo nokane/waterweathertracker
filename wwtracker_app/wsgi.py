5dc82186168801141fe3c193c126e55eca7e4d80
Niall O'Kane <okanenm@gmail.com>
2015-09-25 16:57:51 -0700

    Point elastic beanstalk to correct settings

--- wwtracker_app/wsgi.py
+++ wwtracker_app/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 sys.path.insert(0, '/opt/python/current/app')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
