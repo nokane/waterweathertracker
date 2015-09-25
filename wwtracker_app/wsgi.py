9c08d962e91bcea973bb5cfccf7a244eb9ff47c6
Niall O'Kane <okanenm@gmail.com>
2015-09-25 16:37:33 -0700

    Make changes for elasticbeanstalk deployment

--- wwtracker_app/wsgi.py
+++ wwtracker_app/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wwtracker_app.settings")
-
 application = get_wsgi_application()
