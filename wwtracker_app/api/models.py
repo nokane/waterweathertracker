6abab187ab3d8b75946a28e564254a50a5a9fef5
Niall O'Kane <okanenm@gmail.com>
2015-10-16 15:02:05 -0700

    Restructure app architecture

--- wwtracker_app/api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class State(models.Model):
-    name = models.TextField(max_length=255)
-    abbr = models.TextField(max_length=255)
-    class Meta:
-        db_table = 'states'
