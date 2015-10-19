c8c58c745d9f69ca8c99e78e6e338317b162ba51
Niall O'Kane <okanenm@gmail.com>
2015-10-19 14:25:16 -0700

    Setup ui-router to /map

--- static/js/wwtracker.js
+++ static/js/wwtracker.js
@@ -1,3 +1,15 @@
 var wwtracker = angular.module('wwtracker', [
       'ui.router'
     ]);
+
+wwtracker.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/map');
+    
+    $stateProvider
+        .state('map', {
+            url: '/map',
+            templateUrl: 'static/templates/map/map.html'
+        })
+                
+});
