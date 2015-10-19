584d45b55a73bf661392f100782a46be0128e86c
Niall O'Kane <okanenm@gmail.com>
2015-10-19 14:53:55 -0700

    Initialize MapController

---
+++ static/js/map/controllers/map.controller.js
@@ -0,0 +1,8 @@
+angular
+  .module('wwtracker.map.controllers', [])
+  .controller('MapController', MapController);
+
+  MapController.$inject = ['$scope'];
+
+  function MapController($scope) {
+  }
