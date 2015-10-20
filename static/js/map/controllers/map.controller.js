e355e3e48b838a5aff09e109021109770555218b
Niall O'Kane <okanenm@gmail.com>
2015-10-20 13:41:56 -0700

    Render the map from the Map controller

--- static/js/map/controllers/map.controller.js
+++ static/js/map/controllers/map.controller.js
@@ -2,7 +2,16 @@ angular
   .module('wwtracker.map.controllers', [])
   .controller('MapController', MapController);
 
-  MapController.$inject = ['$scope'];
+MapController.$inject = ['$scope', 'Map'];
 
-  function MapController($scope) {
-  }
+function MapController($scope, Map) {
+
+  var mapOptions = {
+        zoom: 5,
+        center: new google.maps.LatLng(37.7699298, -122.4469157),
+  };
+  $scope.markers = [];
+  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+};
+
