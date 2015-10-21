b3fefa1138b3cc79b6e45c35e574db8e8a7d96e0
Niall O'Kane <okanenm@gmail.com>
2015-10-21 14:39:26 -0700

    Change DB engine

--- wwtracker_app/settings/prod.py
+++ wwtracker_app/settings/prod.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.environ['RDS_DB_NAME'],
         'USER': os.environ['RDS_USERNAME'],
         'PASSWORD': os.environ['RDS_PASSWORD'],
