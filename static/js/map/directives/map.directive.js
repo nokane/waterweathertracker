067bb65d99f0f0869f4ffbf23787e252c2cdf990
Niall O'Kane <okanenm@gmail.com>
2015-10-29 13:01:37 -0700

    Configure the 'waterlist' directive

--- static/js/map/directives/map.directive.js
+++ static/js/map/directives/map.directive.js
@@ -10,3 +10,16 @@ wwtracker.directive('statelist', function () {
     templateUrl: 'static/templates/map/dropdown.html',
   }
 });
+
+wwtracker.directive('waterlist', function() {
+  return {
+    restrict: 'EA',
+    scope: {
+        statewater: "=",
+        currentstate: "=",
+        eventHandler: '&onClick'
+    },
+    transclude: true,
+    templateUrl: 'static/templates/map/waterList.html',
+  }
+});
