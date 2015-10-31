e5ce1a896aa045d85ce3fed32f00dc4b2d5818cc
Niall O'Kane <okanenm@gmail.com>
2015-10-30 19:20:21 -0700

    Implement additional functionality to Water service

--- static/js/water/services/water.service.js
+++ static/js/water/services/water.service.js
@@ -5,10 +5,39 @@ function Water($http) {
     };
     var queryState = function(state) {
       return $http.get('api/water/' + state)
+    };
+    var queryWater = function(waterId) {
+      return $http.get('api/measurement/' + waterId);
+    };
+    var recentMeasure = function(measurements) {
+      var index = 0;
+      var mostRecent = new Date(measurements[0].measured_at)
+      for (var i = 1; i < measurements.length; i++) {
+        var ithMeasurement = new Date(measurements[i].measured_at);
+        if (ithMeasurement > mostRecent) {
+          mostRecent = ithMeasurement;
+          index = i;
+        }
+      }
+      return measurements[index];
     }
+    var prepareGraph = function(measurements, graph) {
+      graph.labels = [];
+      graph.data = [[]];
+      for (var i = 0; i < measurements.length; i++) {
+        var date = new Date(new Date(measurements[i].measured_at));
+        var dateLabel = "" + date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+        graph.labels.push(dateLabel);
+        graph.data[0].push(measurements[i].value);
+      }
+      return graph;
+    };
     var Water = {
       allWater: allWater,
-      queryState: queryState
+      queryState: queryState,
+      queryWater: queryWater,
+      recentMeasure: recentMeasure,
+      prepareGraph: prepareGraph
     };
 
     return Water;
